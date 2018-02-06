07b14ccb048ee62a963df14b88ff148bd98ee513
Will James <wjames@thoughtworks.com>
2018-02-06 10:36:18 -0600

    Adding names of consumer and producer policies to stack outputs

--- publishing/publish-to-s3.js
+++ publishing/publish-to-s3.js
@@ -1,5 +1,7 @@
 const { promisifyAll } = require('bluebird');
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
 
 const s3 = promisifyAll(new AWS.S3());
 
@@ -10,7 +12,10 @@ const exitWithError = (err) => {
   process.exitCode = 1;
 }
 
+const input = fs.createReadStream(path.resolve(__dirname, '..', 'src', 'stack.yml'));
+
 s3.putObjectAsync({
   Bucket: 'safya',
-  Key: `versions/${version}/stack.yml`
+  Key: `versions/${version}/stack.yml`,
+  Body: input
 }).catch(exitWithError);
